1c6af29b0407ea4c020798aab64c750dcca4635e
Areeba Mansoor <159564271+Areeba-Mansoor@users.noreply.github.com>
2026-06-03 16:47:49 +0500

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       return handleError("email & password must be required");
     }
     try {
-      const url = "https://authentication-mern-self.vercel.app";
+      const url = "https://authentication-mern-self.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
